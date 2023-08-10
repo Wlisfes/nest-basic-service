81125b48be7f514d8fea5f945934e1fef770d1d8
妖雨纯 <876451336@qq.com>
2023-08-10 18:46:10 +0800

    ✨ feat(Fair中间件): 修改ip挂载

--- src/middleware/fair/fair.middleware.ts
+++ src/middleware/fair/fair.middleware.ts
@@ -1,11 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common'
-import { Request, Response, NextFunction } from 'express'
 import * as client from 'request-ip'
 
 @Injectable()
 export class FairMiddleware implements NestMiddleware {
-	use(request: Request, response: Response, next: NextFunction) {
-		request.ip = client.getClientIp(request)
+	use(request: any, response: any, next: Function) {
+		request.clientIP = client.getClientIp(request)
 		next()
 	}
 }
