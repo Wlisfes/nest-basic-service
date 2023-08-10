909fa8490325401c49970bc9dea0f3d008eb2feb
妖雨纯 <876451336@qq.com>
2023-08-10 15:51:44 +0800

    ✨ feat(邮件模块): 基本打通邮件发送

---
+++ src/mailer-module/nodemailer/nodemailer.provider.ts
@@ -0,0 +1,52 @@
+const nodemailer = require('nodemailer')
+import * as handlebar from 'handlebars'
+import * as mjml from 'mjml'
+import * as fs from 'fs'
+import * as path from 'path'
+
+export interface NodemailerOption {
+	host: string
+	port: number
+	secure: boolean
+	user: string
+	password: string
+}
+export interface CustomizeOption {
+	from: string
+	to: string
+	subject: string
+	html: string
+}
+
+export async function createNodemailer(option: NodemailerOption) {
+	return new nodemailer.createTransport({
+		host: option.host,
+		port: option.port,
+		secure: option.secure ?? true,
+		auth: {
+			user: option.user,
+			pass: option.password
+		}
+	})
+}
+
+export function customNodemailer(transporter: any, mailOption: CustomizeOption) {
+	return new Promise((resolve, reject) => {
+		return transporter.sendMail(mailOption, (error, info) => {
+			if (error) {
+				reject(error)
+			} else {
+				resolve(info)
+			}
+			return transporter.close()
+		})
+	})
+}
+
+/**读取自定义模板**/
+export async function readNodemailer(option: Record<string, any> = {}) {
+	const register = handlebar.compile(
+		fs.readFileSync(path.join(process.cwd(), './public/template/register.mjml'), 'utf8')
+	)
+	return mjml(register(option)).html
+}
--- src/mailer-module/nodemailer/nodemailer.service.ts
+++ src/mailer-module/nodemailer/nodemailer.service.ts
@@ -1,35 +1,33 @@
 import { Injectable } from '@nestjs/common'
-
-const nodemailer = require('nodemailer')
+import { CoreService } from '@/core/core.service'
+import { EntityService } from '@/core/entity.service'
+import { createNodemailer, customNodemailer, readNodemailer } from './nodemailer.provider'
 
 @Injectable()
-export class NodemailerService {
+export class NodemailerService extends CoreService {
+	constructor(private readonly entity: EntityService) {
+		super()
+	}
+
+	/**读取自定义模板**/
+	public async readCustomize() {}
+
 	/**自定义发送**/
 	public async httpCustomizeNodemailer() {
-		let mailOptions = {
-			from: 'limvcfast@gmail.com',
-			to: '876451336@qq.com',
-			subject: '温馨提示',
-			text: 'That was easy!',
-			html: `欢迎注册情雨随风的妖雨录, 您的验证码是: <b>123456</b> 有效时间30分钟`
-		}
-
-		// const transporter = nodemailer.createTransport({
-		// 	host: 'smtp.gmail.com',
-		// 	port: 587,
-		// 	secure: false,
-		// 	requireTLS: true,
-		// 	auth: {
-		// 		user: '',
-		// 		pass: ''
-		// 	}
-		// })
-		// transporter.sendMail(mailOptions, function (error, info) {
-		// 	if (error) {
-		// 		console.log(error)
-		// 	} else {
-		// 		console.log('Email sent: ' + info.response)
-		// 	}
-		// })
+		return await createNodemailer({
+			host: 'smtp.qq.com',
+			port: 465,
+			secure: true,
+			user: '',
+			password: ''
+		}).then(async transporter => {
+			const html = await readNodemailer({ code: '896543' })
+			return await customNodemailer(transporter, {
+				from: '"妖雨纯" <>',
+				to: '',
+				subject: '温馨提示',
+				html: html
+			})
+		})
 	}
 }
