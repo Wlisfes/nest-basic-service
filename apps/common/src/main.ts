7b554444ec5aeae73ad16f93d80e92eaed1538c5
妖雨纯 <876451336@qq.com>
2023-10-26 16:11:21 +0800

    ✨ feat(Base): 临时存储

--- apps/common/src/main.ts
+++ apps/common/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core'
-import { CommonModule } from './common.module'
+import { ValidationPipe } from '@nestjs/common'
+import { CommonModule } from '@common/common.module'
+import * as express from 'express'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
 	const app = await NestFactory.create(CommonModule)
-	await app.listen(3000)
+	const port = Number(process.env.BASIC_PORT ?? 5050)
+
+	//允许跨域
+	app.enableCors()
+	//解析body参数
+	app.use(cookieParser())
+	app.use(express.json())
+	app.use(express.urlencoded({ extended: true }))
+	//全局注册验证管道
+	app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+	//监听端口服务
+	await app.listen(3000, () => {
+		console.log('Common服务启动:', `http://localhost:${port}`, `http://localhost:${port}/api-doc`)
+	})
 }
 bootstrap()
--- apps/nodemailer/src/main.ts
+++ apps/nodemailer/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { NodemailerModule } from './nodemailer.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { NodemailerModule } from '@nodemailer/nodemailer.module'
+import * as express from 'express'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(NodemailerModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(NodemailerModule)
+	//允许跨域
+	app.enableCors()
+	//解析body参数
+	app.use(cookieParser())
+	app.use(express.json())
+	app.use(express.urlencoded({ extended: true }))
+	//全局注册验证管道
+	app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+	//监听端口服务
+	await app.listen(3000)
 }
-bootstrap();
+bootstrap()
