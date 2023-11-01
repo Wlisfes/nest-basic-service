f4cf20547caaafff132e59260f4a76f7bccd3d03
妖雨纯 <876451336@qq.com>
2023-11-02 00:00:42 +0800

    ✨ feat(captchar): 完善校验凭证接口

--- apps/captchar/src/browser/browser.service.ts
+++ apps/captchar/src/browser/browser.service.ts
@@ -42,7 +42,7 @@ export class BrowserService extends CustomService {
 				secret: data.appSecret,
 				data: { session: session, appId: data.appId }
 			})
-			const job = await firstValueFrom(this.client.send({ cmd: 'create_job_reducer' }, {
+			await firstValueFrom(this.client.send({ cmd: 'create_job_reducer' }, {
 				session,
 				token,
 				appId: data.appId,
@@ -51,8 +51,6 @@ export class BrowserService extends CustomService {
 				nickname: data.customer.nickname,
 				status: 'none'
 			}))
-
-			console.log(`job:`, job)
 			await this.customeCreate(this.tableCaptcharRecord, {
 				appId: data.appId,
 				appName: data.name,
@@ -82,40 +80,35 @@ export class BrowserService extends CustomService {
 					message: '应用已被禁用'
 				})
 			})
-			return await divineParseJwtToken(state.token, {
-				secret: state.appSecret,
-				code: HttpStatus.BAD_REQUEST
-			}).then(async ({ session, appId }) => {
-				await this.validator(this.tableCaptcharRecord, {
-					message: 'token记录不存在',
-					join: { alias: 'tb' },
-					where: new Brackets(qb => {
-						qb.where('tb.session = :session', { session })
-					})
-				}).then(async data => {
-					await divineCatchWherer(['success', 'failure', 'invalid'].includes(data.status), {
-						message: 'token凭证已失效'
-					})
+			await this.validator(this.tableCaptcharRecord, {
+				message: 'token记录不存在',
+				join: { alias: 'tb' },
+				where: new Brackets(qb => {
+					qb.where('tb.session = :session', { session: state.session })
 				})
-
-				return { check: true }
+			}).then(async data => {
+				await divineCatchWherer(['success', 'failure', 'invalid'].includes(data.status), {
+					message: 'token凭证已失效'
+				})
+				return data
 			})
+			try {
+				return await divineParseJwtToken(state.token, {
+					secret: state.appSecret,
+					code: HttpStatus.BAD_REQUEST
+				}).then(async jt => {
+					await divineCatchWherer(jt.session !== state.session, {
+						message: 'token验证失败'
+					})
+					await this.customeUpdate(this.tableCaptcharRecord, { session: state.session }, { status: 'success' })
+					return await divineResult({ check: true })
+				})
+			} catch (e) {
+				await this.customeUpdate(this.tableCaptcharRecord, { session: state.session }, { status: 'failure' })
+				throw new HttpException(e.message, e.status)
+			}
 		} catch (e) {
 			throw new HttpException(e.message, e.status)
 		}
-
-		// return await this.validator(this.tableCaptcharAppwr, {
-		// 	message: '应用不存在',
-		// 	where: new Brackets(qb => {
-		// 		qb.where('tb.appId = :appId', { appId: state.appId })
-		// 		qb.andWhere('tb.status IN(:...status)', { status: ['activated', 'disable'] })
-		// 	})
-		// }).then(async data => {
-		// 	await divineCatchWherer(data.status === 'disable', {
-		// 		message: '应用已被禁用'
-		// 	})
-
-		// 	return data
-		// })
 	}
 }
