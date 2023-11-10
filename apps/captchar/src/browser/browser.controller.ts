e24fd8b2dfbcb5a58153e580b7ed736e4d9ecfa5
妖雨纯 <876451336@qq.com>
2023-11-10 22:48:24 +0800

    ✨ feat(captchar): 临时存储

--- apps/captchar/src/browser/browser.controller.ts
+++ apps/captchar/src/browser/browser.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Post, Body, Headers } from '@nestjs/common'
+import { Inject, Controller, Post, Body, Headers } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { BrowserService } from '@captchar/browser/browser.service'
 import { ApiDecorator } from '@/decorator/compute.decorator'
 import { TableCaptcharRecord } from '@/entity/tb-common.captchar__record'
 import * as http from '@captchar/interface/browser.resolver'
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
+import { Logger } from 'winston'
 
 @ApiTags('验证码校验模块')
 @Controller('browser')
 export class BrowserController {
-	constructor(private readonly browserService: BrowserService) {}
+	constructor(private readonly browserService: BrowserService, @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {}
 
 	@Post('/authorize')
 	@ApiDecorator({
@@ -16,6 +18,7 @@ export class BrowserController {
 		response: { status: 200, description: 'OK', type: TableCaptcharRecord }
 	})
 	public async httpAuthorizeReducer(@Headers() headers, @Body() body: http.AuthorizeReducer) {
+		this.logger.info(BrowserController.name, { body })
 		return await this.browserService.httpAuthorizeReducer(body, headers.origin)
 	}
 
