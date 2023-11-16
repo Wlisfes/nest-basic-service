f98ec6cba69ff214bba6c32e0325a1d0bc55bd1d
妖雨纯 <876451336@qq.com>
2023-11-16 12:42:38 +0800

    ✨ feat(configer): 修改环境变量配置

--- src/utils/utils-configer.ts
+++ src/utils/utils-configer.ts
@@ -9,7 +9,7 @@ export enum CustomConfiger {
 
 /**自定义注入配置**/
 export function CustomProvider(): Record<string, any> {
-	const env = process.env.NODE_ENV as keyof typeof CustomConfiger
+	const env = process.env.NODE_ENV.trim() as keyof typeof CustomConfiger
 	return yaml.load(fs.readFileSync(path.join(__dirname, `../../../${env}.yaml`), 'utf8'))
 }
 
