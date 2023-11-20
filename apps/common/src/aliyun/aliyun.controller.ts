08bec2c12b06f16519bedc05a48d0a81e3a2cfdd
妖雨纯 <876451336@qq.com>
2023-11-20 22:51:19 +0800

    ✨ feat(aliyun): 新增oss授权接口

--- apps/common/src/aliyun/aliyun.controller.ts
+++ apps/common/src/aliyun/aliyun.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { AliyunService } from '@common/aliyun/aliyun.service'
+import { ApiDecorator } from '@/decorator/compute.decorator'
+import * as dataBase from '@/entity'
 
+@ApiTags('阿里云模块')
 @Controller('aliyun')
-export class AliyunController {}
+export class AliyunController {
+	constructor(private readonly aliyunService: AliyunService) {}
+
+	@Post('/create/storage/authorize')
+	@ApiDecorator({
+		operation: { summary: '创建OSS-STS临时鉴权' },
+		response: { status: 200, description: 'OK' },
+		authorize: { login: true, error: true }
+	})
+	public async httpCreateStorageAuthorize(@Request() request: { user: dataBase.TableCustomer }) {
+		return await this.aliyunService.httpCreateStorageAuthorize(request.user.uid)
+	}
+}
--- apps/common/src/aliyun/aliyun.service.ts
+++ apps/common/src/aliyun/aliyun.service.ts
@@ -1,11 +1,61 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Inject, HttpStatus, HttpException } from '@nestjs/common'
 import { CustomService } from '@/service/custom.service'
 import { CacheCustomer } from '@/cache/cache-customer.service'
 import { DataBaseService } from '@/service/database.service'
+import { OSS_CLIENT, OSS_STS_CLIENT } from '@common/aliyun/aliyun.provider'
+import { moment, divineCatchWherer, divineBufferToStream } from '@/utils/utils-plugin'
+import { divineIntNumber, divineResult } from '@/utils/utils-common'
+import { custom } from '@/utils/utils-configer'
+import * as Client from 'ali-oss'
 
 @Injectable()
 export class AliyunService extends CustomService {
-	constructor(private readonly customer: CacheCustomer, private readonly dataBase: DataBaseService) {
+	constructor(
+		private readonly cacheCustomer: CacheCustomer,
+		private readonly dataBase: DataBaseService,
+		@Inject(OSS_CLIENT) protected readonly client: Client,
+		@Inject(OSS_STS_CLIENT) protected readonly stsClient: Client.STS
+	) {
 		super()
+	}
+
+	/**文件流转换**/
+	public async createStream(file, pathFolder = 'static') {
+		const fieldName = file.originalname
+		const suffix = fieldName.slice(fieldName.lastIndexOf('.') + 1).toLowerCase()
+		const fileId = await divineIntNumber()
+		const fileName = [fileId, suffix].join('.')
+		const folder = ['basic', pathFolder, moment().format('YYYY-MM'), fileName].join('/')
+		return { fileId, fileName, folder, fieldName, suffix, fileStream: await divineBufferToStream(file.buffer) }
+	}
+
+	/**删除文件**/
+	public async deleteFiler(path: string) {
+		return await this.client.delete(path)
+	}
+
+	/**创建OSS临时授权**/
+	public async httpCreateStorageAuthorize(uid: string) {
+		try {
+			const rolearn = custom.aliyun.oss.rolearn
+			const sessionname = custom.aliyun.oss.sessionname
+			const result = await this.stsClient.assumeRole(rolearn, '', 7200, sessionname)
+			await divineCatchWherer(result.res.statusCode !== HttpStatus.OK, {
+				message: '授权失败',
+				code: HttpStatus.NOT_IMPLEMENTED
+			})
+			return await divineResult({
+				interval: 7000,
+				endpoint: custom.aliyun.oss.endpoint,
+				bucket: custom.aliyun.oss.bucket,
+				region: custom.aliyun.oss.region,
+				accessKeyId: result.credentials.AccessKeyId,
+				accessKeySecret: result.credentials.AccessKeySecret,
+				token: result.credentials.SecurityToken,
+				expire: moment(result.credentials.Expiration).format('YYYY-MM-DD HH:mm:ss')
+			})
+		} catch (e) {
+			throw new HttpException(e.message, HttpStatus.BAD_REQUEST)
+		}
 	}
 }
