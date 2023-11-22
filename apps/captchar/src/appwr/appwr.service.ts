9a573653de4c9cb7af5e14fa4d4183fef8a2d925
妖雨纯 <876451336@qq.com>
2023-11-22 18:39:58 +0800

    ✨ feat(captchar): 完善应用编辑接口

--- apps/captchar/src/appwr/appwr.service.ts
+++ apps/captchar/src/appwr/appwr.service.ts
@@ -6,8 +6,9 @@ import { CacheCustomer } from '@/cache/cache-common.service'
 import { CacheAppwr } from '@/cache/cache-captchar.service'
 import { DataBaseService } from '@/service/database.service'
 import { divineIntNumber, divineIntStringer, divineResult, divineWherer } from '@/utils/utils-common'
-import { divineClientSender } from '@/utils/utils-plugin'
+import { divineCatchWherer } from '@/utils/utils-plugin'
 import { custom } from '@/utils/utils-configer'
+import * as dataBase from '@/entity'
 import * as http from '@captchar/interface/appwr.resolver'
 
 @Injectable()
@@ -31,7 +32,7 @@ export class AppwrService extends CustomService {
 			where: new Brackets(qb => {
 				qb.where('tb.uid = :uid', { uid })
 				qb.andWhere('tb.name = :name', { name: state.name })
-				qb.andWhere('tb.status != :status', { status: 'delete' })
+				qb.andWhere('tb.status IN(:...status)', { status: ['activated', 'disable'] })
 			})
 		})
 		//查询应用数量
@@ -52,20 +53,44 @@ export class AppwrService extends CustomService {
 	}
 
 	/**编辑应用**/
-	public async httpUpdateCaptcharAppwr(state: http.UpdateCaptcharAppwr, uid: string) {}
+	public async httpUpdateCaptcharAppwr(state: http.UpdateCaptcharAppwr, uid: string) {
+		return await this.cacheAppwr.checkCache(state.appId, ['disable']).then(async data => {
+			await divineCatchWherer(data.uid !== uid, {
+				message: '应用不存在'
+			})
+			//验证重复性
+			await this.validator(this.dataBase.tableCaptcharAppwr, {
+				join: { alias: 'tb' },
+				where: new Brackets(qb => {
+					qb.where('tb.uid = :uid', { uid })
+					qb.andWhere('tb.name = :name', { name: state.name })
+					qb.andWhere('tb.status IN(:...status)', { status: ['activated', 'disable'] })
+				})
+			}).then(async data => {
+				return await divineCatchWherer(data && data.appId !== state.appId, {
+					message: `${state.name}已存在`
+				})
+			})
+			await this.customeUpdate(
+				this.dataBase.tableCaptcharAppwr,
+				{ appId: state.appId },
+				{
+					name: state.name
+				}
+			)
+			return await divineResult({ message: '编辑成功' })
+		})
+	}
 
 	/**应用列表**/
 	public async httpColumnCaptcharAppwr(state: http.ColumnCaptcharAppwr, uid: string) {
-		return await this.customeAndCountr(this.dataBase.tableCaptcharAppwr, {
-			join: { alias: 'tb' },
-			where: new Brackets(qb => {
-				qb.where('tb.uid = :uid', { uid })
-			}),
-			order: { createTime: 'DESC' },
-			skip: (state.page - 1) * state.size,
-			take: state.size
-		}).then(async ({ list, total }) => {
-			return await divineResult({ total, list })
+		return await this.customeBuilder(this.dataBase.tableCaptcharAppwr, qb => {
+			qb.leftJoinAndMapOne('tb.customer', dataBase.TableCustomer, 'customer', 'customer.uid = tb.uid')
+			qb.skip((state.page - 1) * state.size)
+			qb.take(state.size)
+			return qb.getManyAndCount()
+		}).then(([list = [], total = 0]) => {
+			return divineResult({ total, list, size: state.size, page: state.page })
 		})
 	}
 }
