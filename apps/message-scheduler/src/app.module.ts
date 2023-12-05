1c68bd5c10335df108b84806303fbecfdce3f239
妖雨纯 <876451336@qq.com>
2023-12-05 23:37:15 +0800

    ✨ feat(message): 新增uni注入

--- apps/message-scheduler/src/app.module.ts
+++ apps/message-scheduler/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AppController } from '@message-scheduler/app.controller'
+import { AppService } from '@message-scheduler/app.service'
+import { createUniClientProvider } from '@message-scheduler/uni.provider'
 
 @Module({
 	imports: [],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [createUniClientProvider(), AppService]
 })
 export class AppModule {}
--- apps/message-scheduler/src/uni.provider.ts
+++ apps/message-scheduler/src/uni.provider.ts
@@ -1,1 +1,15 @@
 import UniSMS from 'unisms'
+import { custom } from '@/utils/utils-configer'
+export const UNI_CLIENT = 'UNI_CLIENT'
+
+/**创建Uni实例**/
+export function createUniClientProvider() {
+	return {
+		provide: UNI_CLIENT,
+		async useFactory() {
+			return new UniSMS({
+				accessKeyId: custom.message.provide.uni.accessKeyId
+			})
+		}
+	}
+}
