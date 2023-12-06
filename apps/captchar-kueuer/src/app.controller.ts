b92927aab0790b8cf84acd1f013dc0f8fb602883
妖雨纯 <876451336@qq.com>
2023-12-06 23:52:26 +0800

    ✨ feat(Base): 删除TCP配置

--- apps/captchar-kueuer/src/app.controller.ts
+++ apps/captchar-kueuer/src/app.controller.ts
@@ -1,19 +1,7 @@
 import { Controller } from '@nestjs/common'
-import { MessagePattern } from '@nestjs/microservices'
 import { AppService } from '@captchar-kueuer/app.service'
-import { custom } from '@/utils/utils-configer'
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
-
-	@MessagePattern({ cmd: custom.captchar.kueuer.instance.cmd.httpCreateJobKueuer })
-	public async httpCreateJobKueuer(data: Record<string, never>) {
-		return await this.appService.httpCreateJobKueuer(data)
-	}
-
-	@MessagePattern({ cmd: custom.captchar.kueuer.instance.cmd.httpUpdateJobKueuer })
-	public async httpUpdateJobKueuer(data: Record<string, never>) {
-		return await this.appService.httpUpdateJobKueuer(data.jobId, data.option)
-	}
 }
--- apps/common/src/app.controller.ts
+++ apps/common/src/app.controller.ts
@@ -1,16 +1,9 @@
 import { Controller, Get } from '@nestjs/common'
-import { MessagePattern } from '@nestjs/microservices'
 import { AppService } from '@common/app.service'
-import { custom } from '@/utils/utils-configer'
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
-
-	@MessagePattern({ cmd: custom.common.instance.cmd.httpConnect })
-	public async httpConnect(data: Record<string, never>) {
-		return data
-	}
 
 	@Get()
 	async httpHelloer() {
