105d544c0bd82a4f11bc32c598ead3781ed50fa6
妖雨纯 <876451336@qq.com>
2023-12-08 23:45:52 +0800

    ✨ feat(message-kueuer): 完善短信队列配置

--- apps/message-kueuer/src/app.module.ts
+++ apps/message-kueuer/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { LoggerModule } from '@/module/logger.module'
+import { ConfigerModule } from '@/module/configer.module'
+import { DatabaseModule } from '@/module/database.module'
+import { BullModule } from '@nestjs/bull'
+import { AppController } from '@message-kueuer/app.controller'
+import { AppService } from '@message-kueuer/app.service'
+import { custom } from '@/utils/utils-configer'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigerModule,
+		LoggerModule.forRoot({ name: 'Message-Kueuer' }),
+		BullModule.forRoot({
+			prefix: custom.redis.prefix,
+			redis: {
+				host: custom.redis.host,
+				port: custom.redis.port,
+				password: custom.redis.password,
+				db: custom.redis.db
+			}
+		}),
+		BullModule.registerQueue({
+			name: custom.message.kueuer.bull.name,
+			defaultJobOptions: {
+				removeOnComplete: custom.message.kueuer.bull.removeOnComplete,
+				removeOnFail: custom.message.kueuer.bull.removeOnFail
+			}
+		}),
+		DatabaseModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
--- apps/message-kueuer/src/main.ts
+++ apps/message-kueuer/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { Transport, MicroserviceOptions } from '@nestjs/microservices'
+import { AppModule } from '@message-kueuer/app.module'
+import { custom } from '@/utils/utils-configer'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+		transport: Transport.TCP,
+		options: {
+			port: custom.message.kueuer.port
+		}
+	})
+	await app.listen().then(e => {
+		console.log('Message-kueuer服务启动:', `TCP: ${custom.message.kueuer.port}`)
+	})
 }
 bootstrap()
