6e71da41fd9a5ad588b3870b2362d7e8c93f2bd2
妖雨纯 <876451336@qq.com>
2023-12-08 23:35:32 +0800

    ✨ feat(message): 添加基础模块集成

--- apps/message/src/app.module.ts
+++ apps/message/src/app.module.ts
@@ -1,12 +1,33 @@
-import { Module } from '@nestjs/common'
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
 import { HttpModule } from '@nestjs/axios'
+import { LoggerMiddleware } from '@/middleware/logger.middleware'
+import { LoggerModule } from '@/module/logger.module'
+import { ConfigerModule } from '@/module/configer.module'
+import { CustomizeModule } from '@/module/customize.module'
+import { DatabaseModule } from '@/module/database.module'
+import { CacheModule } from '@/module/cache.module'
+import { CustomerModule } from '@common/customer/customer.module'
+import { AliyunModule } from '@common/aliyun/aliyun.module'
 import { AppController } from '@message/app.controller'
 import { AppService } from '@message/app.service'
 import { createUniClientProvider } from '@message-scheduler/uni.provider'
 
 @Module({
-	imports: [HttpModule],
+	imports: [
+		LoggerModule.forRoot({ name: 'Message' }),
+		HttpModule,
+		ConfigerModule,
+		CustomizeModule,
+		DatabaseModule,
+		CacheModule,
+		CustomerModule,
+		AliyunModule
+	],
 	controllers: [AppController],
 	providers: [createUniClientProvider(), AppService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggerMiddleware).forRoutes('*')
+	}
+}
