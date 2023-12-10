0de816777f436d672371fd60e1b0b7c8d70d899c
妖雨纯 <876451336@qq.com>
2023-12-10 22:11:39 +0800

    ✨ feat(message-scheduler): 新增调度服务配置

--- apps/message-scheduler/src/main.ts
+++ apps/message-scheduler/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { Transport, MicroserviceOptions } from '@nestjs/microservices'
+import { AppModule } from '@message-scheduler/app.module'
+import { custom } from '@/utils/utils-configer'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+		transport: Transport.TCP,
+		options: {
+			port: custom.message.scheduler.port
+		}
+	})
+	await app.listen().then(e => {
+		console.log('Message-Scheduler服务启动:', `TCP: ${custom.message.scheduler.port}`)
+	})
 }
 bootstrap()
