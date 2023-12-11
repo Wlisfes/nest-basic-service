0c6e1c2571eef8e7715bf21b68e0dc0030316ea7
妖雨纯 <876451336@qq.com>
2023-12-11 17:22:47 +0800

    🦄 refactor(Base): 修改环境变量配置

--- src/utils/utils-configer.ts
+++ src/utils/utils-configer.ts
@@ -3,16 +3,10 @@ import * as yaml from 'js-yaml'
 import * as path from 'path'
 import * as ip from 'ip'
 
-export enum CustomConfiger {
-	development = 'development',
-	production = 'production'
-}
-
 /**自定义注入配置**/
 export function CustomProvider(): Record<string, any> {
 	const ipv4 = ip.address()
-	const env = process.env.NODE_ENV.trim() as keyof typeof CustomConfiger
-	const configer = yaml.load(fs.readFileSync(path.join(__dirname, `../../../${env}.yaml`), 'utf8'))
+	const configer = yaml.load(fs.readFileSync(path.join(__dirname, `../../../development.yaml`), 'utf8'))
 	return Object.assign({ ipv4 }, configer)
 }
 
